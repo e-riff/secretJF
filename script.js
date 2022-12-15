405db50031ca501ceae29ddb4b7606d640ef4f3b
e-riff <emeric.riff@gmail.com>
2022-12-15 12:28:12 +0100

    Hohohoho

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ wind.addEventListener("click", function () {
         flocons = document.querySelectorAll("span.truc");
         for (let flocon of flocons) {
             flocon.innerHTML = "JF"
-            flocon.style.fontSize = 80 + "px";
+            flocon.style.fontSize = 60 + "px";
             flag = true;
         }
     } else {
